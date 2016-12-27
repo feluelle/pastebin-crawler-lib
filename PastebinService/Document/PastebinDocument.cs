0fc8e32b66b100bb64e2d86aa458d7c4d762f96a
Felix Uellendall <felix.uellendall@hotmail.de>
2016-12-27 22:34:58 +0100

    Added existed project code

---
+++ PastebinService/Document/PastebinDocument.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HtmlAgilityPack;
+
+namespace Pastebin.Service.Document
+{
+    public class PastebinDocument
+    {
+        public HtmlDocument HtmlDocument { get; set; }
+        public PastebinDocument(string htmlString)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(htmlString);
+
+            HtmlDocument = doc;
+        }
+
+        public IEnumerable<HtmlAttribute> GetArchive()
+        {
+            return HtmlDocument.DocumentNode
+                .SelectSingleNode("//table[@class='maintable']")
+                .Descendants("a")
+                .Select(a => a.Attributes["href"]);
+        }
+
+        public IEnumerable<HtmlAttribute> GetSmallArchive()
+        {
+            return HtmlDocument.DocumentNode
+                .SelectSingleNode("//ul[@class='right_menu']")
+                .Descendants("a")
+                .Select(a => a.Attributes["href"]);
+        }
+    }
+}
