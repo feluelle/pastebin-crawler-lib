3dec110773df49bfc463c886c457f9446512e123
feluelle <felix.uellendall@hotmail.de>
2017-01-29 07:49:24 +0100

    Changed PastebinDocumentDownloader to PastebinCrawler + changed from static to non-static Changed some method param names Changed namespaces: - from Pastebin.Service.Document to feluelle.Pastebin.Document - from Pastebin.Service.DocumentDownloader to feluelle.Pastebin.Crawler

---
+++ PastebinService/PastebinCrawler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace feluelle.Pastebin.Crawler
+{
+    public class PastebinCrawler
+    {
+        const string PASTEBIN_URL = "http://www.pastebin.com";
+
+        WebClient wc;
+
+        public PastebinCrawler()
+        {
+            wc = new WebClient();
+        }
+
+        public string DownloadArchive()
+        {
+            return wc.DownloadString(PASTEBIN_URL + "/archive");
+        }
+
+        public string DownloadEntry(string entryId)
+        {
+            return wc.DownloadString(PASTEBIN_URL + "/raw" + entryId);
+        }
+
+        public void DownloadEntries(IEnumerable<string> entries, Action<int, string> afterEach)
+        {
+            for (int i = 0; i < entries.Count(); i++)
+                afterEach(i, DownloadEntry(entries.ElementAt(i)));
+        }
+    }
+}
---
+++ PastebinService/PastebinDocument.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HtmlAgilityPack;
+
+namespace feluelle.Pastebin.Document
+{
+    public class PastebinDocument
+    {
+        public HtmlDocument HtmlDocument { get; set; }
+        public PastebinDocument(string htmlString)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(htmlString);
+
+            HtmlDocument = doc;
+        }
+
+        public IEnumerable<string> GetArchive()
+        {
+            return findUrlsIn("//table[@class='maintable']");
+        }
+
+        public IEnumerable<string> GetSmallArchive()
+        {
+            return findUrlsIn("//ul[@class='right_menu']");
+        }
+
+        private IEnumerable<string> findUrlsIn(string xpath)
+        {
+            return from aNode in HtmlDocument.DocumentNode
+                   .SelectSingleNode(xpath)
+                   .Descendants("a")
+                   select aNode.Attributes["href"].Value;
+        }
+    }
+}
